a90bcfcf24dc1ea3d5420b1cbde24cf9efcd146d
Manu <manu21181@gmail.com>
2019-12-07 01:28:55 +0100

    Mejoras en restringir la búsqueda del tesoro, estéticas y funcionales (MapBox)

--- static/restrict.js
+++ static/restrict.js
@@ -1,54 +1,65 @@
- /*
- var map = new ol.Map({
-        target: 'map',
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.OSM()
-            })
-        ],
-        view: new ol.View({
-            center: ol.proj.fromLonLat([long, lat]),
-            zoom: 6
-        })
-    });
-*/
-//    console.log(map.getView().calculateExtent(map.getSize()));
+//console.log(map.getView().calculateExtent(map.getSize()));
+
+ var bounds = [
+[-90.04728500751165, 30.68392799015035], // Southwest coordinates
+[-50.91058699000139, 55.87764500765852]  // Northeast coordinates
+];
 
 
- function transform(extent) {
-  return ol.proj.transformExtent(extent, 'EPSG:4326', 'EPSG:3857');
-}
-
-var extents = {
-  India: transform([68.17665, 7.96553, 97.40256, 35.49401]),
-  Argentina: transform([-73.41544, -55.25, -53.62835, -21.83231]),
-  Nigeria: transform([2.6917, 4.24059, 14.57718, 13.86592]),
-  Sweden: transform([11.02737, 55.36174, 23.90338, 69.10625])
-};
+mapboxgl.accessToken ='pk.eyJ1IjoibWFudXBsLTk3IiwiYSI6ImNrM3Vmd2JvNzBjM3kzZ3Brc253cTl0b3EifQ.06G3hmSOFoDuxGu38AT76w';
 
 
-var key = 'pk.eyJ1IjoibWFudXBsLTk3IiwiYSI6ImNrM3Vmd2JvNzBjM3kzZ3Brc253cTl0b3EifQ.06G3hmSOFoDuxGu38AT76w';
-
-var base = new ol.layer.Tile({
-  source: new ol.source.OSM()
-});
-
-var overlay = new ol.layer.Tile({
-  extent: extents.Argentina,
-  source: new ol.source.TileJSON({
-    url: 'https://api.tiles.mapbox.com/v4/mapbox.world-blue.json?secure&access_token=' + key,
-    crossOrigin: 'anonymous'
-  })
-});
-
-var map = new ol.Map({
-  layers: [base, overlay],
+var map = new mapboxgl.Map({
+  style: 'mapbox://styles/mapbox/streets-v11',
+  maxBounds: bounds, // Sets bounds as max
+  container: 'map',
   target: 'map',
-  view: new ol.View({
-    center: [-0.119687322282424, 51.50328025],
-    zoom: 4
-  })
+  center: [-68.8590201, 45.709097],
+  zoom: 6
 });
 
 
-//overlay.setExtent(extents[event.target.id]);
+map.on('load', function () {
+
+
+map.addLayer({
+  'id': 'maine',
+  'type': 'fill',
+  'source': {
+  'type': 'geojson',
+  'data': {
+  'type': 'Feature',
+  'geometry': {
+  'type': 'Polygon',
+  'coordinates': [[[-67.13734351262877, 45.137451890638886],
+    [-66.96466, 44.8097],
+    [-68.03252, 44.3252],
+    [-69.06, 43.98],
+    [-70.11617, 43.68405],
+    [-70.64573401557249, 43.090083319667144],
+    [-70.75102474636725, 43.08003225358635],
+    [-70.79761105007827, 43.21973948828747],
+    [-70.98176001655037, 43.36789581966826],
+    [-70.94416541205806, 43.46633942318431],
+    [-71.08482, 45.3052400000002],
+    [-70.6600225491012, 45.46022288673396],
+    [-70.30495378282376, 45.914794623389355],
+    [-70.00014034695016, 46.69317088478567],
+    [-69.23708614772835, 47.44777598732787],
+    [-68.90478084987546, 47.184794623394396],
+    [-68.23430497910454, 47.35462921812177],
+    [-67.79035274928509, 47.066248887716995],
+    [-67.79141211614706, 45.702585354182816],
+    [-67.13734351262877, 45.137451890638886]]]
+    }
+  }
+},
+
+  'layout': {},
+  'paint': {
+  'fill-color': '#088',
+  'fill-opacity': 0.2
+    }
+  });
+});
+
